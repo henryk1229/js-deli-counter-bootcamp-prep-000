80bfa3ec53f13bf9eb70a7ccf0f76e3225cdb8c6
Henry Koehler <henryk1229@gmail.com>
2019-02-07 15:43:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 function takeANumber(line, name) { line.push(`${name}`); return (`Welcome, ${name}. You are number ${line.length} in line.`) } 
 function nowServing(line) { if (line.length === 0) { return "There is nobody waiting to be served!" } return `Currently serving ${line.shift()}.` } 
-function currentLine(line) { if (line.length === 0) { return "The line is currently empty." } return `The line is currently ${line.length + 1}. ${line}.` } 
+function currentLine(line) { if (line.length === 0) { return "The line is currently empty." } }
